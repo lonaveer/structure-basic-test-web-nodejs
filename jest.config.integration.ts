164d584ce1129e2999c09a343018eaaa1531fea9
lonaveer <lonaveer@gmail.com>
2024-03-08 09:50:11 +0700

    init

---
+++ jest.config.integration.ts
@@ -0,0 +1,14 @@
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	verbose: true, 						// show test details
+	collectCoverage: true, 				// ใช้เพื่อกำหนดว่า Jest ควรคำนวณความครอบคลุมของโค้ดหรือไม่เมื่อทำการทดสอบ
+    collectCoverageFrom: [
+		"<rootDir>/src/**/*.ts", 		// ทำการ Test Coverage : ไฟล์ TypeScript ทั้งหมดในโฟลเดอร์ src
+    	"!<rootDir>/src/**/*.test.ts", 	// ยกเว้นไฟล์ TypeScript ประเภท test ทั้งหมด
+    ],
+	testMatch: [
+		'<rootDir>/src/test/integration-test/**/*.test.ts'
+	],									// ทำการรันเฉพาะไฟล์ Test ที่อยู่ในโฟลเดอร์ test/integration-test
+};
+
